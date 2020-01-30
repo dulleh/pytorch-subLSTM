4c564c0092e9ef26236ac5459f2d017d4dd23aab
dulleh <dulleftovers@gmail.com>
2020-01-30 16:56:23 +0000

    Print everything

--- src/subLSTM/basic/nn.py
+++ src/subLSTM/basic/nn.py
@@ -79,7 +79,7 @@ class SubLSTMFunction(Function):
             ctx.saved_variables[4],
             ctx.saved_variables[5],
             ctx.saved_variables[6],
-            ctx.saved_variables[7])#,
+            ctx.saved_variables[7]).cuda()#,
             #ctx.saved_variables[8])
         d_old_h, d_input, d_weights, d_bias, d_old_cell = outputs
         return d_input, d_weights, d_bias, d_old_h, d_old_cell
