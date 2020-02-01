39b4513b3d0b9101dba0609c9839518180b81e13
dulleh <dulleftovers@gmail.com>
2020-02-01 23:30:33 +0000

    dont assume 3 dimensional

--- src/subLSTM/basic/sublstm.cu
+++ src/subLSTM/basic/sublstm.cu
@@ -57,7 +57,7 @@ std::vector<torch::Tensor> forward_cuda(
     torch::Tensor bias,
     torch::Tensor old_h,
     torch::Tensor old_cell) {
-  printf("input (%d, %d, %d)\n", input.size(0), input.size(1), input.size(2));
+  printf("input (%d, %d)\n", input.size(0), input.size(1));
   printf("input (%d, %d)\n", old_h.size(0), old_h.size(1));
 
   auto X = torch::cat({old_h, input}, /*dim=*/1);
