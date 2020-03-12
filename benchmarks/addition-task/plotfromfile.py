8d86a23f51649c1ba4621e83f881f0feb5ac4a47
dulleh <dulleftovers@gmail.com>
2020-03-12 15:33:26 +0000

    Add AoT fused to comparison graphs

--- benchmarks/addition-task/plotfromfile.py
+++ benchmarks/addition-task/plotfromfile.py
@@ -22,17 +22,19 @@ from utils import train, test, drawepochs
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    batchsize, seqlen, trainingsize, numepochs = 16, 4, 80, 9
+    batchsize, seqlen, trainingsize, numepochs = 4, 4, 80, 9
     parser = argparse.ArgumentParser(description='Addition task')
     path_to_this = os.path.abspath(os.path.dirname(__file__))
     cuda_v1_file_name = 'CUDA_v1_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
     cuda_fused_file_name = 'CUDA_fused_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
+    cuda_fused_AOT_file_name = 'CUDA_fused_AOT_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
     python_v1_file_name = 'python_v1_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
     python_fused_file_name = 'python_fused_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
     lstm_unfused_file_name = 'LSTM_unfused_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
     lstm_fused_file_name = 'LSTM_fused_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
     cuda_v1_save_path = os.path.join(path_to_this, cuda_v1_file_name)
     cuda_fused_save_path = os.path.join(path_to_this, cuda_fused_file_name)
+    cuda_fused_AOT_save_path = os.path.join(path_to_this, cuda_fused_AOT_file_name)
     python_v1_save_path = os.path.join(path_to_this, python_v1_file_name)
     python_fused_save_path = os.path.join(path_to_this, python_fused_file_name)
     lstm_unfused_save_path = os.path.join(path_to_this, lstm_unfused_file_name)
@@ -40,6 +42,7 @@ if __name__ == '__main__':
 
     cudav1times = np.loadtxt(cuda_v1_save_path, delimiter=',')
     cudafusedtimes = np.loadtxt(cuda_fused_save_path, delimiter=',')
+    cudafusedaottimes = np.loadtxt(cuda_fused_AOT_save_path, delimiter=',')
     pythonv1times = np.loadtxt(python_v1_save_path, delimiter=',')
     pythonfusedtimes = np.loadtxt(python_fused_save_path, delimiter=',')
     lstmunfusedtimes = np.loadtxt(lstm_unfused_save_path, delimiter=',')
@@ -49,6 +52,7 @@ if __name__ == '__main__':
     #plt.plot(np.arange(1, len(pythonfusedtimes)+1, 1), pythonfusedtimes, label='subLSTM Py (run alongside fused)', color='tab:orange')
     plt.plot(np.arange(1, len(cudav1times)+1, 1), cudav1times, label='subLSTM CUDA (unfused, FML)', color='tab:green')
     plt.plot(np.arange(1, len(cudafusedtimes)+1, 1), cudafusedtimes, label='subLSTM CUDA (fused, FML)', color='tab:red')
+    plt.plot(np.arange(1, len(cudafusedaottimes)+1, 1), cudafusedaottimes, label='subLSTM CUDA (fused, FML, AOT)', color='tab:pink')
     plt.plot(np.arange(1, len(lstmunfusedtimes)+1, 1), lstmunfusedtimes, label='LSTM Py (unfused)', color='tab:purple')
     plt.plot(np.arange(1, len(lstmfusedtimes)+1, 1), lstmfusedtimes, label='LSTM (fused+)', color='tab:brown')
     plt.legend()
