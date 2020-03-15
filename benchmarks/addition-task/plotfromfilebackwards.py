d598505815ac034fafb7a7c52057d379046312ad
dulleh <dulleftovers@gmail.com>
2020-03-15 14:51:50 +0000

    Add backwards comparison plots

---
+++ benchmarks/addition-task/plotfromfilebackwards.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+import sys
+import os
+import argparse
+import time
+import csv
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+
+sys.path.insert(0, '../../src/')
+sys.path.insert(0, '../')
+
+from wrappers import init_model
+from utils import train, test, drawepochs
+
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    batchsize, seqlen, trainingsize, numepochs = 4, 4, 80, 9
+    parser = argparse.ArgumentParser(description='Addition task')
+    path_to_this = os.path.abspath(os.path.dirname(__file__))
+
+    python_file_name = 'backward_python_v1_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
+    cuda_v1_file_name = 'backward_CUDA_v1_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
+    cuda_fused_file_name = 'backward_CUDA_fused_batch{}_seq{}_train{}_epochs{}.csv'.format(batchsize, seqlen, trainingsize, numepochs)
+
+    python_v1_save_path = os.path.join(path_to_this, python_v1_file_name)
+    cuda_v1_save_path = os.path.join(path_to_this, cuda_v1_file_name)
+    cuda_fused_save_path = os.path.join(path_to_this, cuda_fused_file_name)
+
+    pythonv1times = np.loadtxt(python_v1_save_path, delimiter=',')
+    cudav1times = np.loadtxt(cuda_v1_save_path, delimiter=',')
+    cudafusedtimes = np.loadtxt(cuda_fused_save_path, delimiter=',')
+
+    plt.suptitle('Avg. Forward Time per Epoch Vs Hidden Units with Batch Size {}, Seq. Length {}, Training Size {} across {} epochs'.format(batchsize, seqlen, trainingsize, numepochs))
+    plt.plot(np.arange(1, len(pythonv1times)+1, 1), pythonv1times, label='subLSTM Py (unfused)', color='tab:blue')
+    plt.plot(np.arange(1, len(cudav1times)+1, 1), cudav1times, label='subLSTM CUDA (unfused, FML, AOT)', color='tab:green')
+    plt.plot(np.arange(1, len(cudafusedtimes)+1, 1), cudafusedtimes, label='subLSTM CUDA (fused, FML, AOT)', color='tab:red')
+    #plt.plot(np.arange(1, len(lstmunfusedtimes)+1, 1), lstmunfusedtimes, label='LSTM Py (unfused)', color='tab:purple')
+    #plt.plot(np.arange(1, len(lstmfusedtimes)+1, 1), lstmfusedtimes, label='LSTM (fused+)', color='tab:brown')
+    plt.legend()
+    plt.xlabel('Hidden units')
+    plt.ylabel('Average Time (s)')
+    plt.show()
